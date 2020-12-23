9417443427033dcc2798ca1b86011062775c5abf
Concrete18 <michaelericson19@gmail.com>
2020-12-23 00:54:07 -0500

    Updated Readme for fork

--- Main.py
+++ Main.py
@@ -1,25 +1,28 @@
+from keras.preprocessing import image
+from matplotlib import pyplot as plt
+import matplotlib as mpl
+import numpy as np
+import Secreit
 import tkinter.filedialog
 import os
-import pprint
+import matplotlib.rcsetup as rcsetup
+print(rcsetup.all_backends)
 
-class Phase_Check:
+class Stage_Check:
 
 
-    def __init__(self):
-        self.debug = 0
-        self.ask_for_dir = 0
-        self.images_location = 'Cell Images'
+    def __init__(self, debug=0, use_graph=1, ask_for_dir=0, def_images_location=''):
+        self.debug = debug
+        self.use_graph = use_graph
+        self.ask_for_dir = ask_for_dir
+        self.def_images_location = def_images_location
 
 
     def main(self, cell_image):
-        print(cell_image.name)
-        if self.debug:
-            return
-        from keras.preprocessing import image
-        from matplotlib import pyplot as plt
-        import matplotlib as mpl
-        import numpy as np
-        import Secreit
+        '''
+        Modified Secreit codde.
+        Returns dictionary with analyzed information.
+        '''
         model = Secreit.vgg_model('weights.hdf5')
         img = image.load_img(cell_image.path)
         data_set = {}
@@ -27,57 +30,70 @@ class Phase_Check:
         plt.imshow(img)
 
         predict = Secreit.predict(img, model)
+
         filename = cell_image.name
         d_percent = str(round(predict[0]*100))
         e_percent = str(round(predict[1]*100))
         p_percent = str(round(predict[2]*100))
 
+        Stages = {
+            'D':'Diestrus',
+            'E':'Estrus',
+            'P':'Proestrus'
+                }
+        data_set['D'] = d_percent
+        data_set['E'] = e_percent
+        data_set['P'] = p_percent
+        data_set['Probable Stage'] = Stages[list(filter(lambda x:x[1] == max(data_set.values()), data_set.items()))[0][0]]
         data_set['Filename'] = filename
-        data_set['D'] = f'{d_percent}%'
-        data_set['E'] = f'{e_percent}%'
-        data_set['P'] = f'{p_percent}%'
 
-        imgd = image.array_to_img(Secreit.Cam(np.array(img), "D", model))
-        imge = image.array_to_img(Secreit.Cam(np.array(img), "E", model))
-        imgp = image.array_to_img(Secreit.Cam(np.array(img), "P", model))
+        if self.use_graph == 1:
+            imgd = image.array_to_img(Secreit.Cam(np.array(img), "D", model))
+            imge = image.array_to_img(Secreit.Cam(np.array(img), "E", model))
+            imgp = image.array_to_img(Secreit.Cam(np.array(img), "P", model))
 
-        mpl.rcParams['figure.figsize'] = [20, 4]
+            mpl.rcParams['figure.figsize'] = [20, 4]
 
-        plt.subplot(1,4,1)
-        plt.imshow(img)
-        plt.title(d_percent + e_percent + p_percent, fontsize = 25)
+            plt.subplot(1,4,1)
+            plt.imshow(img)
+            plt.title(f'D:{d_percent}% E:{e_percent}% + P:{p_percent}%', fontsize = 25)
 
-        plt.subplot(1,4,2)
-        plt.imshow(imgd)
-        plt.title("D place", fontsize = 25)
+            plt.subplot(1,4,2)
+            plt.imshow(imgd)
+            plt.title("D place", fontsize = 25)
 
-        plt.subplot(1,4,3)
-        plt.imshow(imge)
-        plt.title("E place", fontsize = 25)
+            plt.subplot(1,4,3)
+            plt.imshow(imge)
+            plt.title("E place", fontsize = 25)
 
-        plt.subplot(1,4,4)
-        plt.imshow(imgp)
-        plt.title("P place", fontsize = 25)
+            plt.subplot(1,4,4)
+            plt.imshow(imgp)
+            plt.title("P place", fontsize = 25)
+
+            plt.show
         return data_set
 
 
     def image_loop(self):
         '''
-        Loops through images in specific folder
+        Loops through images in specific folder.
         '''
-        if self.debug:
-            print('Running debug version of code. If you are expecting results, change self.debug to 0.')
         if self.ask_for_dir:
             tkinter.Tk().withdraw() # hides blank tkinter window that pop up otherwise
-            self.images_location  = tkinter.filedialog.askdirectory(initialdir="C:/", title="Select Save Directory")
-        print(f'Phase check on images located in {self.images_location}')
+            self.def_images_location  = tkinter.filedialog.askdirectory(initialdir="C:/", title="Select Save Directory")
+        print(f'Stage check on images located in {self.def_images_location}')
         data_list = []
-        for cell_image in os.scandir(self.images_location):
+        for cell_image in os.scandir(self.def_images_location):
             data_list.append(self.main(cell_image))
-        pprint.pprint(data_list)
+            if self.debug:
+                break
+        print('')
+        for entry in data_list:
+            print(f"{entry['Filename']}\nD:{entry['D']}% E:{entry['E']}% P:{entry['P']}%")
+            print(f"Probable Stage: {entry['Probable Stage']}")
         # TODO Add .csv output format for results.
 
 
 if __name__ ==  "__main__":
-    App = Phase_Check()
+    App = Stage_Check(debug=0, use_graph=0, ask_for_dir=0, def_images_location='Cell Images')
     App.image_loop()
